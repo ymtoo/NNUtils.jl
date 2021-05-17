056dde79d2917c1c67b5c80ac218e1bcada81494
ymtoo <ymtoo86@gmail.com>
2021-05-17 15:34:06 +0800

    docs(sincconv): update docstrings

--- src/layers/sincconv.jl
+++ src/layers/sincconv.jl
@@ -18,11 +18,58 @@ end
     SincConv(fs, filter, in => out, σ=identity; stride=1, pad=0, dilation=1, init=initcutofffreqs)
 
 Convolutinal layer with parameterized sinc functions which implement band-pass filters. `fs` is the
-sampling rate. Details can refer to https://github.com/FluxML/Flux.jl/blob/master/src/layers/conv.jl 
+sampling rate. For more details, you can refer to https://github.com/FluxML/Flux.jl/blob/master/src/layers/conv.jl 
 
 # Examples
-```julia
+```julia-repl
+julia> using Flux, NNUtils
 
+julia> fs = 9600f0
+9600.0f0
+
+julia> model = SincConv(fs, (200, 1), 1=>8, identity)
+SincConv(9600.0, (200, 1), 1=>8)
+
+julia> params(model) |> length
+2
+
+julia> params(model)[1] |> size
+(1, 8)
+
+julia> params(model)[2] |> size
+(1, 8)
+
+julia> x = randn(Float32, 4800, 1, 1, 16)
+4800×1×1×16 Array{Float32, 4}:
+[:, :, 1, 1] =
+ 1.3832613
+ 0.42255098
+ ⋮
+ 0.3134887
+
+[:, :, 1, 2] =
+ 2.0712132
+ 0.13467419
+ ⋮
+
+ julia> model(x)
+ 4601×1×8×16 Array{Float32, 4}:
+ [:, :, 1, 1] =
+   -3498.0605
+  -17983.041
+   20055.25
+   -1296.112
+       ⋮
+    1844.9044
+   10672.3125
+  -12256.136
+ 
+ [:, :, 2, 1] =
+    521.0923
+   8794.186
+   4179.6655
+  -4084.1067
+      ⋮
 ```
 
 # Reference
