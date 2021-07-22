00aa7c4407f85e489de861b41aaadc1d5512a5a3
ymtoo <ymtoo86@gmail.com>
2021-07-22 11:35:17 +0800

    fix: use Zygote.jacobian

--- src/saliencymaps.jl
+++ src/saliencymaps.jl
@@ -1,14 +1,14 @@
-function jacobian(f, x)
-    y = f(x)
-    n = length(y)
-    m = length(x)
-    T = eltype(y)
-    J = Array{T,2}(undef, n, m)
-    for i in 1:n
-        J[i, :] .= gradient(x -> f(x)[i], x)[1] |> vec
-    end
-    return J
-end
+# function jacobian(f, x)
+#     y = f(x)
+#     n = length(y)
+#     m = length(x)
+#     T = eltype(y)
+#     J = Array{T,2}(undef, n, m)
+#     for i in 1:n
+#         J[i, :] .= gradient(x -> f(x)[i], x)[1] |> vec
+#     end
+#     return J
+# end
 
 function _saliencymap(δf, s)
     δf |> 
@@ -32,14 +32,14 @@ end
 function saliencymap(::Gradient, f, x)
     #d1, d2, d3 = size(x,1), size(x,2), size(x,3)
     #f(x) = x |> reshape(x, d1 * d2 * d3, :) |> g
-    δf = jacobian(f, x)
+    δf = jacobian(f, x)[1]
     _saliencymap(δf, size(x)[1:end-1])
 end
 
 function saliencymap(sm::SmoothGradient, f, x)
     #d1, d2, d3 = size(x,1), size(x,2), size(x,3)
     #f(x) = x |> reshape(x, d1 * d2 * d3, :) |> g
-    δf = jacobian(f, x)
+    δf = jacobian(f, x)[1]
     σ = sm.noiselevel * (maximum(x) - minimum(x)) / 100
     for i ∈ 2:sm.n
         δf += jacobian(f, x .+ σ .* randn.(eltype(δf)))
